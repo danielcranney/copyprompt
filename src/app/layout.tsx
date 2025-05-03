8c70f56d25c3fb7f78e2e3ad5e1f3603ee43e92d
Daniel Cranney <hello@danielcranney.com>
2025-05-03 18:12:57 +0100

    Chore: Meta Data Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Copyprompt",
+  title: "Copyprompt - Roleplay prompts to get better results from AI",
   description: "Roleplay prompts to get better results from AI",
 };
 
